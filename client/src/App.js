3fab7072e4549ad820ade45e8f5a6a06634a58f1
krelle02 <krelle02@hotmail.com>
2022-05-09 21:13:49 +0200

    api

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import Nav from "./Components/Nav";
-import Dashboard from "./Components/Dashboard";
+import Nav from "./Views/Components/Nav";
+import Dashboard from "./Views/Components/Dashboard";
 import Collections from "./Views/My bots/Collections";
 function App() {
   return (
--- client/src/Views/My bots/Components/CollectionCard.js
+++ client/src/Views/My bots/Components/CollectionCard.js
@@ -1,26 +1,25 @@
+import axios from "axios";
 
+//the api request is bugged for some reason
 export default function CollectionCard(props) {
-
-  const onClickHandler =  async () => {
-    const reponse = await fetch("/posts/create", {
-      method: "POST",
+  const onClickHandler = async () => {
+    const reponse = await axios.post("posts/create", {
       body: JSON.stringify({
-          name: props.title,
-          code: "gie-nfqq-jnv",
-          time: 5000,
+        name: props.title,
+        code: "gie-nfqq-jnv",
+        time: 5000,
       }),
-      headers: {
-          "Content-type": "application/json; charset=UTF-8"
-      }
-  })
-    window.alert(`Your bot has been created with: ${reponse}`)
-  }
+    });
+    window.alert(`Your bot has been created with: ${reponse}`);
+  };
 
   return (
-    <div onClick={onClickHandler} className=" flex flex-col items-center justify-around  rounded-md bg-white shadow-xl ">
+    <div
+      onClick={onClickHandler}
+      className=" flex flex-col items-center justify-around  rounded-md bg-white shadow-xl "
+    >
       <h3 className=""> {props.title} </h3>
       <img className="h-auto w-4/5" src={props.src}></img>
-      
     </div>
   );
 }
